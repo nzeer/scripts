7becd8bdbeae3f07beef9b75c57601170f71e96c
nZeeR <nzeer@me.com>
2023-11-16 07:42:08 -0500

    updated and made better

--- backup/backup.sh
+++ backup/backup.sh
@@ -6,16 +6,16 @@
 ####################################
 
 # What to backup. 
-backup_files="/var/lib/docker"
+backup_files="/home/nzeer"
 
 # Where to backup to.
-dest="/home/backups"
+dest="/home/backup"
 
 # Create archive filename.
-day=$(date +%A)
-time=$(date +%H%M)
+datetime=$(date +%F)
+#time=$(date +%H%M)
 #file=$(file)
-archive_file="$day-$time-docker.tgz"
+archive_file="$datetime-home.tgz"
 archive_path="$dest/$archive_file"
 
 echo $archive_file
@@ -24,7 +24,7 @@ echo $archive_path
 # backup info
 backup_user="nzeer"
 backup_server="172.16.0.70"
-backup_storage_location="~"
+backup_storage_location="/home/backup"
 backup_destination="$backup_user@$backup_server:$backup_storage_location"
 
 # Print start status message.
@@ -33,7 +33,7 @@ date
 echo
 
 # Backup the files using tar.
-sudo tar czf $archive_path $backup_files
+sudo tar --exclude /home/nzeer/.cache --exclude /home/nzeer/.rustup --exclude /home/nzeer/.local --exclude /home/nzeer/repos --exclude /home/nzeer/docker -cvzf $archive_path $backup_files
 sudo rsync $archive_path $backup_destination
 # Print end status message.
 echo
