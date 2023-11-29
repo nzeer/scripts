e6d199c6dc2cf9d5e3b089649f056351a36a5085
nZeeR <nzeer@me.com>
2023-11-29 17:53:09 -0500

    added run script

---
+++ dynamic_inventory/run.sh
@@ -0,0 +1,15 @@
+# scan network
+cd /home/nzeer/repos/semaphore-repo/gather_facts
+ansible-inventory -i nmap.yaml --list >> hosts.json
+
+# copy hosts.json to dynamic inventory
+cp hosts.json /home/nzeer/repos/scripts/dynamic_inventory
+
+# convert hosts.json to inventory file
+cd /home/nzeer/repos/scripts/dynamic_inventory
+python json2ini2.py
+
+# collect os information
+ansible all -m setup -a 'filter=ansible_distribution*' -i inventory > os-data.json
+
+
