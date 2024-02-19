25b9572c0586b0501746519b494f4443ae7085d5
nZeeR <nzeer@me.com>
2024-02-19 18:01:02 -0500

    added initial commit for library functions for storing/loading pickle files

--- python/lib/libpickle.py
+++ python/lib/libpickle.py
@@ -1,6 +1,29 @@
 import pickle
+
+GLOBAL_CONFIG = { 
+    'dbfile': 'examplePickle',
+}
  
-def storeData():
+def store_data(pickle_file: str, db: dict = {})-> bool:
+    is_good_write = False
+    # Its important to use binary mode
+    with open(pickle_file, 'ab') as db_file:
+        # source, destination
+        pickle.dump(db, db_file)
+        is_good_write = True
+    return is_good_write
+
+def load_data(pickle_file:str)-> bool:
+    is_good_read = False    
+    # for reading also binary mode is important
+    with open(pickle_file, 'rb') as db_file:
+        db = pickle.load(db_file)
+        for keys in db:
+            print('\t\t', keys, '=>', db[keys])
+        is_good_read = True
+    return is_good_read 
+
+if __name__ == '__main__':
     # initializing data to be stored in db
     Omkar = {'key' : 'Omkar', 'name' : 'Omkar Pathak',
     'age' : 21, 'pay' : 40000}
@@ -11,22 +34,8 @@ def storeData():
     db = {}
     db['Omkar'] = Omkar
     db['Jagdish'] = Jagdish
-     
-    # Its important to use binary mode
-    dbfile = open('examplePickle', 'ab')
-     
-    # source, destination
-    pickle.dump(db, dbfile)                    
-    dbfile.close()
- 
-def loadData():
-    # for reading also binary mode is important
-    dbfile = open('examplePickle', 'rb')    
-    db = pickle.load(dbfile)
-    for keys in db:
-        print(keys, '=>', db[keys])
-    dbfile.close()
- 
-if __name__ == '__main__':
-    storeData()
-    loadData()
+    
+    if store_data(GLOBAL_CONFIG['dbfile'], db):
+        print('Data stored successfully\n')
+    if load_data(GLOBAL_CONFIG['dbfile']):
+        print('\nData loaded successfully')
