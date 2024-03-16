2b7a1442deaa247cf103072f2f78834da1d16bbc
nZeeR <nzeer@me.com>
2024-03-15 21:56:48 -0400

    updated shell script

--- sysprep/prep-system.sh
+++ sysprep/prep-system.sh
@@ -1,4 +1,6 @@
-# 209.160.33.152
+echo "========================"
+echo "Beginning sysprep"
+echo "========================"
 echo "Disabling firewalld and fapolicyd."
 systemctl stop firewalld
 systemctl disable firewalld
@@ -14,4 +16,5 @@ python3 ./prep-network. && echo "Network configured."
 python3 ./prep-splunk.py && echo "Splunk configured."
 python3 ./prep-rsyslog.py && echo "Rsyslog configured."
 python3 ./prep-hosts.py && echo "Hosts configured."
+pyhton3 ./prep-yum-clean-update.py && echo "Yum updated."
 echo "System prepped."
